4a8b88305a2af6854727882a26073721f11b65ce
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-05-16 17:01:40 +0000

    Created component to create a new task

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import CreateTask from './create_tasks/CreateTasks';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import List from './list/List';
+import Button from 'react-bootstrap/Button';
 
 class Tasks extends Component {
 
@@ -13,6 +15,7 @@ class Tasks extends Component {
       this.loadTasks = this.loadTasks.bind(this);
    }
 
+   
    async loadTasks(){
       let response = await fetch('http://localhost:3001/tasks');
       const tasks = await response.json();
@@ -29,10 +32,12 @@ class Tasks extends Component {
          <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
             <p className="title">To-do</p>
             <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done != true)}/>
+            <CreateTask loadTasks={this.loadTasks}/>
          </Col>
          <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
             <p className="title">Done</p>
             <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done != false)}/>
+            <Button variant="red" className="float-right remove_tasks_btn">Remove all tasks</Button>
          </Col>
          </Row>
       );
