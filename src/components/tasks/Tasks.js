f534eef70ea83398006f5c4aa5cd45965b18473e
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-05-16 16:57:11 -0300

    add remove all done tasks feature

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -22,6 +22,13 @@ class Tasks extends Component {
       this.setState({ tasks: tasks });
    }
 
+   async deleteDoneTasks() {
+      if (window.confirm(`Are you sure you want to delete all done tasks ?`)) {
+        await fetch(`http://localhost:3001/tasks`, {method: 'DELETE'});
+        this.loadTasks();
+      }
+    }
+
    componentDidMount(){
       this.loadTasks();
    }
@@ -37,7 +44,7 @@ class Tasks extends Component {
          <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
             <p className="title">Done</p>
             <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done != false)}/>
-            <Button variant="red" className="float-right remove_tasks_btn">Remove all tasks</Button>
+            <Button variant="red" className="float-right remove_tasks_btn" onClick={() => this.deleteDoneTasks()} >Remove all tasks</Button>
          </Col>
          </Row>
       );
